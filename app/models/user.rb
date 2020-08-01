8a58b97db8a142a32397de5e96606a7bdae0841a
tk8931 <kabuto7737@gmail.com>
2020-08-01 14:01:54 +0000

    Finish editing-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true    
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   # 渡された文字列のハッシュ値を返します。
   def User.digest(string)
     cost = 
@@ -40,6 +39,7 @@ def authenticated?(remember_token)
   return false if remember_digest.nil?
   BCrypt::Password.new(remember_digest).is_password?(remember_token)
 end
+
   # ユーザーのログイン情報を破棄します。
   def forget
     update_attribute(:remember_digest, nil)
